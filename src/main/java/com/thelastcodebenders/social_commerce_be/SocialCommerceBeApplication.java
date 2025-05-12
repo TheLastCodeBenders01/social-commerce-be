2bd68aa9716694b3a84ac6a8572e7326fc0cfd1a
itzmhizterlouis <itzmhizterlouis@gmail.com>
2025-05-12 22:42:24 +0100

    Project Bootstrap

---
+++ src/main/java/com/thelastcodebenders/social_commerce_be/SocialCommerceBeApplication.java
@@ -0,0 +1,13 @@
+package com.thelastcodebenders.social_commerce_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialCommerceBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialCommerceBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thelastcodebenders/social_commerce_be/SocialCommerceBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thelastcodebenders.social_commerce_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialCommerceBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
