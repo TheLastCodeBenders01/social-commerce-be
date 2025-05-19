f64707c745f5f90d093c1b1746dd13735c570f38
itzmhizterlouis <itzmhizterlouis@gmail.com>
2025-05-19 22:51:50 +0100

    response push

---
+++ src/main/java/com/thelastcodebenders/social_commerce_be/models/dto/PaymentResponse.java
@@ -0,0 +1,14 @@
+package com.thelastcodebenders.social_commerce_be.models.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class PaymentResponse {
+    private boolean status;
+    private String message;
+    private String reference;
+    private String checkout_url;
+    private String processor;
+}
