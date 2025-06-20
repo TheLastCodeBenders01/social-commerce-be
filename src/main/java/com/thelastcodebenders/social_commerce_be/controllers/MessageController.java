c8170c80d7662a3506fde15cb016a6dedb779e20
itzmhizterlouis <itzmhizterlouis@gmail.com>
2025-06-20 04:50:15 +0100

    deploy documentation

--- src/main/java/com/thelastcodebenders/social_commerce_be/controllers/MessageController.java
+++ src/main/java/com/thelastcodebenders/social_commerce_be/controllers/MessageController.java
@@ -6,6 +6,7 @@ import com.thelastcodebenders.social_commerce_be.models.entities.Message;
 import com.thelastcodebenders.social_commerce_be.models.entities.Room;
 import com.thelastcodebenders.social_commerce_be.services.MessageService;
 import com.thelastcodebenders.social_commerce_be.services.RoomService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -25,21 +26,25 @@ public class MessageController {
     private final MessageService messageService;
     private final RoomService roomService;
 
+    @Operation(summary = "get or create room if not exist")
     @PostMapping("room")
     public Room getOrCreateRoom(@RequestParam(name = "secondUser") UUID secondUser) {
         return roomService.getOrCreateRoom(secondUser);
     }
 
+    @Operation(summary = "send message to room")
     @PostMapping("send/{roomId}")
     public Message sendMessage(@PathVariable UUID roomId, @ModelAttribute MessageRequest request) {
         return messageService.sendMessage(roomId, request);
     }
 
+    @Operation(summary = "get user rooms")
     @GetMapping("user")
     public List<Room> getUserRooms() {
         return roomService.getUserRooms();
     }
 
+    @Operation(summary = "get room messages")
     @GetMapping("room")
     public List<Message> getRoomMessages(
             @RequestParam(name = "roomId") UUID roomId, @RequestParam(name = "pageNumber") int pageNumber, @RequestParam("pageSize") int pageSize
